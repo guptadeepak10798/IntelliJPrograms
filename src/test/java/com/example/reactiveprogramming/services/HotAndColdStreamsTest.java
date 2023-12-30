51d08390b92630fec979971701b6caaddcec6554
DEEPAK GUPTA <deepakgupta7977423290@gmail.com>
2023-12-30 12:21:12 +0530

    Added HotAndColdStrems.java file

--- src/test/java/com/example/reactiveprogramming/services/HotAndColdStreamsTest.java
+++ src/test/java/com/example/reactiveprogramming/services/HotAndColdStreamsTest.java
@@ -23,11 +23,21 @@ public class HotAndColdStreamsTest {
     }
 
     @Test
+    public void colStreamTest() {
+        var numbers = Flux.range(1,10);
+
+        numbers.subscribe(integer -> System.out.println("Subscriber 1 = " + integer));
+        numbers.subscribe(integer -> System.out.println("Subscriber 2 = " + integer));
+    }
+
+    @Test
     @SneakyThrows
     public void hotStreamTest() {
         Flux<Integer> numbers = Flux.range(1, 10)
                 .delayElements(Duration.ofMillis(1000));
+
         ConnectableFlux<Integer> publisher = numbers.publish();
+
         publisher.connect();
 
         publisher.subscribe(integer ->
@@ -42,6 +52,21 @@ public class HotAndColdStreamsTest {
                 System.out.println("subscriber2 = " + integer)
         );
 
+        Thread.sleep(10000);
+    }
+
+    @SneakyThrows
+    @Test
+    public void hotStreamTest1() {
+        var numbers = Flux.range(1,10)
+                .delayElements(Duration.ofMillis(1000));
+
+        ConnectableFlux<Integer> publisher = numbers.publish();
+        publisher.connect();
+
+        publisher.subscribe(integer -> System.out.println("Subscriber 1 = " + integer));
+        Thread.sleep(4000);
+        publisher.subscribe(integer -> System.out.println("Subscriber 2 = " + integer));
         Thread.sleep(10000);
     }
 }
